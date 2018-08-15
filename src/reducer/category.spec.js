259539aba73e296cea3b70c7dad6a7db0392d083
Phillip Kim <phildkim@gmail.com>
2018-08-14 19:16:52 -0700

    fixed test

--- src/reducer/category.spec.js
+++ src/reducer/category.spec.js
@@ -18,27 +18,28 @@ describe('This reducer should', () => {
     expect(newCategory[0].id).toBeDefined();
   });
 
-  xit('update an existing category', () => {
+  it('update an existing category', () => {
+
+    const newCategory = reducer([], actions.categoryCreate(categoryTest));
+    let id = newCategory[0].id;
     let updateTo = {
+      id: id,
       name: 'Promotion',
       budget: '$1000',
     };
 
-    const newCategory = reducer([], actions.categoryCreate(categoryTest));
-    console.log('start', newCategory);
-
     const updatedCategory = reducer(newCategory, actions.categoryUpdate(updateTo));
-    console.log('end', updatedCategory);
-    
+
+    expect(updatedCategory[0].id).toBe(id);
     expect(updatedCategory[0].name).toBe('Promotion');
     expect(updatedCategory[0].budget).toBe('$1000');
-    expect(updatedCategory[0].id).toEqual(categoryTest[0].id);
+    expect(updatedCategory.length).toBe(1);
   });
 
-  xit('delete an existing category', () => {
-    let newCategory = reducer([], actions.categoryCreate(categoryTest));
-    newCategory = reducer(newCategory, actions.categoryDelete(newCategory[0].id));
+  it('delete an existing category', () => {
+    const newCategory = reducer([], actions.categoryCreate(categoryTest));
+    const deleteCategory = reducer(newCategory, actions.categoryDelete(newCategory[0]));
 
-    expect(newCategory.length).toBe(0);
+    expect(deleteCategory.length).toEqual(0);
   });
 });
